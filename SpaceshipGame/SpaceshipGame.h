fa7a6432ad0c2d333a56d5ea0b911f6291749580
jang-taehyun <jth0202@naver.com>
2025-07-21 18:30:46 +0900

    코드 규칙 정립

--- SpaceshipGame/SpaceshipGame.h
+++ SpaceshipGame/SpaceshipGame.h
@@ -21,7 +21,13 @@
 * 3. Scene 관련 규칙
 * 1) Scene 클래스를 상속받은 자식 클래스에서는 반드시 복사 생성자, 이동 생성자, 복사 대입연산자, 이동 대입 연산자를 구현할 것
 * 2) Scene 클래스에서 무조건 Camera 클래스의 인스턴스는 한 개 이상 만들 것
-* 3) Scene 클래스를 상속받은 자식 클래스에서는 반드시 필요한 object, text, ui를 manager를 통해서 생성할 것
+* 3) 새로운 Scene을 프로젝트에 추가할 때의 규칙
+*	3-1) SceneClass를 상속받아서 새로운 Class를 정의할 것
+*	3-2) 새로운 Scene의 ID를 config.cpp 파일에 등록할 것
+*	3-3) SceneClass를 상속받은 새로운 Class를 SceneFactoryClass에 등록할 것
+* 4) Scene 클래스를 상속받은 자식 클래스에서는 반드시 필요한 object, text, ui, sound를 manager를 통해서 생성할 것
+*	4-1) object, text, ui는 load() 함수를 호출하여 생성할 것
+*	4-2) sound는 비트마스크를 통해 필요한 sound의 ID를 표시한 후에 load() 함수를 호출하여 생성할 것 
 * 
 * 4. Shader 관련 규칙
 * 1) Shader 클래스를 상속받은 자식 클래스에서는 반드시 복사 생성자, 이동 생성자, 복사 대입연산자, 이동 대입 연산자를 구현할 것
@@ -42,21 +48,28 @@
 * 1) Model 클래스를 상속받은 자식 클래스에서는 반드시 복사 생성자, 이동 생성자, 복사 대입연산자, 이동 대입 연산자를 구현할 것
 * 2) Model 클래스의 인스턴스를 생성할 때 manager를 통해서 생성을 요청할 것
 * 3) 새로운 Model을 프로젝트에 추가할 때의 규칙
-*	3-1) 새로운 Model을 메모리에 Load 시 ModelLoaderClass를 상속받아서 새로운 Class를 정의할 것
+*	3-1) 새로운 Model을 메모리에 load 시 ModelLoaderClass를 상속받아서 새로운 Class를 정의할 것
 *	3-2) 새로운 Model의 ID를 config.cpp 파일에 등록할 것
 *		- model 파일은 resource 디렉토리에 저장할 것
 *		- model 파일의 상대 경로는 config.cpp 안에 있는 map에 ID와 함께 등록할 것
 *		- model 파일 안에 필요한 texture의 경로는 config.cpp 안에 있는 map에 ID와 함께 등록할 것
 *	3-3) ModelLoaderClass를 상속받은 새로운 Class를 ModelFactoryClass에 등록할 것
 * 
-* 6. Object, UI, Text 관련 규칙
+* 6. Sound 관련 규칙
+* 1) 새로운 sound를 프로젝트에 추가할 때의 규칙
+*	1-1) 새로운 sound의 ID를 config.cpp 파일에 등록할 것
+*		- sound 파일은 resource 디렉토리에 저장할 것
+*		- sound 파일의 상대 경로는 config.cpp 안에 있는 map에 ID와 함께 등록할 것
+*		- sound 파일은 .wav 파일을 사용할 것
+* 
+* 7. Object, UI, Text 관련 규칙
 * 1) Object, UI, Text 클래스의 인스턴스를 생성할 때 manager를 통해서 생성을 요청할 것
 * 2) Object, UI 클래스의 자식 클래스 생성 시, factory에 등록할 것
 * 
-* 7. 객체 생성 관련
+* 8. 객체 생성 관련
 * 1) 싱글톤 패턴 금지
 * 
-* 8. 예외 처리 관련
+* 9. 예외 처리 관련
 * 1) 예외 처리는 assert()를 사용할 것
 * 2) WINAPI, DirectX 관련 API를 사용하는 경우 HRESULT 자료형을 이용해 리턴값을 받고, assert()를 통해 검사할 것
 */
